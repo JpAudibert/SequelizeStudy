5a975a452d8e32323b359e4da8ae739999bc6050
João Pedro Basso Audibert <hotsjoao@gmail.com>
2019-10-20 23:04:32 -0300

    Inicializing the repo, it's already inserting users in the database (POSTGRES)

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "joaopedro",
+  password: "joaopedro",
+  database: "sqlnode",
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+};
